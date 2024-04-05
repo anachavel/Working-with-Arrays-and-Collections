e448eaec996c51a69a35b06a3e02c41c8109a806
AVANADE-CORP\ana.chavez.velasco <ana.chavez.velasco@gmail.com>
2024-04-05 02:16:50 +0200

    no message

--- DisplayRoutes/obj/Debug/net5.0/DisplayRoutes.AssemblyInfo.cs
+++ DisplayRoutes/obj/Debug/net5.0/DisplayRoutes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DisplayRoutes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cd1277166c1f38398e8e208feda3722951628e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DisplayRoutes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DisplayRoutes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
