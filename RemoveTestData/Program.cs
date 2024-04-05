13e0bab295b5feea0213aa2819c2de6d9a51fc1c
AVANADE-CORP\ana.chavez.velasco <ana.chavez.velasco@gmail.com>
2024-04-05 02:37:38 +0200

    Cambios del módulo 4

--- RemoveTestData/Program.cs
+++ RemoveTestData/Program.cs
@@ -3,22 +3,21 @@ using System.Collections.Generic;
 
 namespace Pluralsight.ArraysCollections.Demos
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			BusRoute[] allRoutes = BusRouteRepository.InitializeRoutes();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<BusRoute> allRoutes = BusRouteRepository.InitializeRoutes();
 
-			Console.WriteLine($"Before: There are {allRoutes.Length} routes:");
-			foreach (BusRoute route in allRoutes)
-				Console.WriteLine($"Route: {route}");
+            Console.WriteLine($"Before: There are {allRoutes.Count} routes:");
+            foreach (BusRoute route in allRoutes)
+                Console.WriteLine($"Route: {route}");
 
-			// this isn't really a good way of removing an item - but it happens to work here
-			Array.Resize(ref allRoutes, 4);
+            allRoutes.RemoveAll(route => route.Origin.StartsWith("Test "));
 
-			Console.WriteLine($"\r\nAfter: There are {allRoutes.Length} routes:");
-			foreach (BusRoute route in allRoutes)
-				Console.WriteLine($"Route: {route}");
-		}
-	}
+            Console.WriteLine($"\r\nAfter: There are {allRoutes.Count} routes:");
+            foreach (BusRoute route in allRoutes)
+                Console.WriteLine($"Route: {route}");
+        }
+    }
 }
