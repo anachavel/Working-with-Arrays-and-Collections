4c51649300928bfed320f368c215f4558e49866e
AVANADE-CORP\ana.chavez.velasco <ana.chavez.velasco@gmail.com>
2024-04-05 12:11:16 +0200

    Cambios y añado ArriveAtTerminus()

--- SimulateJourney/PassengerGenerator.cs
+++ SimulateJourney/PassengerGenerator.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace Pluralsight.ArraysCollections.Demos
 {
-	public static class PassengerGenerator
-	{
-		private static int _count = 0;
-		public static Passenger CreatePassenger()
-		{
-			string destination = "Lancaster";
-			return new Passenger($"Person {++_count}", destination);
-		}
-	}
+    public static class PassengerGenerator
+    {
+        private static int _count = 0;
+        public static Passenger CreatePassenger()
+        {
+            string destination = "Lancaster";
+            return new Passenger($"Person {++_count}", destination);
+        }
+    }
 }
